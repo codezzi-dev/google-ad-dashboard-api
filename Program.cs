ec0509fa20988382b06f7ed24785045f29e20a8c
codezzi-dev <codezzi.dev@gmail.com>
2025-05-16 14:30:49 +0600

    removing static data from program.cs

--- Program.cs
+++ Program.cs
@@ -36,11 +36,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapControllers();
 
 app.Run();
